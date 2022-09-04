b24eb44192053d6624c3ede6f9f75e1b83d4264d
Pedro <pedrocastillo24.pc@gmail.com>
2022-09-04 15:41:32 -0300

    ambito puppeteer

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { router } = require("./routes/index");
+const { router } = require("./src/routes/index");
 const cors = require("cors");
 
 const app = express();
--- src/controllers/quotes/ambitoDolarBlue.js
+++ src/controllers/quotes/ambitoDolarBlue.js
@@ -1,15 +1,19 @@
-const { webkit } = require("playwright");
+const puppeteer = require("puppeteer");
 
 async function getValuesAmbito() {
-  const browser = await webkit.launch();
+  /*   const browser = await puppeteer.launch();
   const context = await browser.newContext({ ignoreHTTPSErrors: true });
   const page = await context.newPage();
   await page.goto("https://www.ambito.com/contenidos/dolar.html", {
     timeout: 0,
-  });
+  }); */
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto("https://www.ambito.com/contenidos/dolar.html");
 
-  const buyPrice = await page.textContent(".data-compra");
-  const sellPrice = await page.textContent(".data-venta");
+  const buyPrice = await page.$eval(".data-compra", (e) => e.innerText);
+  const sellPrice = await page.$eval(".data-venta", (e) => e.innerText);
 
   const dataDolarBlue = {
     buy_price: buyPrice,
@@ -19,6 +23,7 @@ async function getValuesAmbito() {
   };
 
   await browser.close();
+
   return dataDolarBlue;
 }
 
