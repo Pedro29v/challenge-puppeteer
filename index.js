3b2874b306991dac863705cc6a9092fc889765d3
Pedro <pedrocastillo24.pc@gmail.com>
2022-09-04 17:52:10 -0300

    use cors

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const { router } = require("./src/routes/index");
-const { CORS_URL } = process.env;
+
+const cors = require("cors");
 const port = process.env.PORT || 3001;
 
 const app = express();
 
-/* app.use((req, res) => {
-  res.header("Access-Control-Allow-Origin", CORS_URL);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET");
-}); */
-
+app.use(
+  cors({ origin: "https://dollar-blue-price.vercel.app/", credentials: true })
+);
 app.use(express.json());
 app.use(router);
 
