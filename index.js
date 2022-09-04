4f9861447a7eda6942d820abae8a7856227187fa
Pedro <pedrocastillo24.pc@gmail.com>
2022-09-04 18:32:18 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { router } = require("./src/routes/index");
-const { CORS_URL } = process.env;
 const cors = require("cors");
 const port = process.env.PORT || 3001;
 
