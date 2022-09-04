2c92fff66a5fff68b7cbd296b165ec21ae583313
Pedro <pedrocastillo24.pc@gmail.com>
2022-09-04 18:03:40 -0300

    again

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const { router } = require("./src/routes/index");
-
+const { CORS_URL } = process.env;
 const cors = require("cors");
 const port = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(
-  cors({ origin: "https://dollar-blue-price.vercel.app/", credentials: true })
-);
+app.use(cors({ origin: CORS_URL, credentials: true }));
 app.use(express.json());
 app.use(router);
 
