483c6f093aab0b87fa79991e5737702c5f6bbfc0
Pedro <pedrocastillo24.pc@gmail.com>
2022-09-06 14:50:03 -0300

    simplificacion de ambito

--- src/controllers/quotes/ambitoDolarBlue.js
+++ src/controllers/quotes/ambitoDolarBlue.js
@@ -1,29 +1,18 @@
-const puppeteer = require("puppeteer");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 async function getValuesAmbito() {
-  /*   const browser = await puppeteer.launch();
-  const context = await browser.newContext({ ignoreHTTPSErrors: true });
-  const page = await context.newPage();
-  await page.goto("https://www.ambito.com/contenidos/dolar.html", {
-    timeout: 0,
-  }); */
-  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-  const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0);
-  await page.goto("https://www.ambito.com/contenidos/dolar.html");
-
-  const buyPrice = await page.$eval(".data-compra", (e) => e.innerText);
-  const sellPrice = await page.$eval(".data-venta", (e) => e.innerText);
+  const { data } = await axios.get(
+    "https://mercados.ambito.com//dolar/informal/variacion"
+  );
 
   const dataDolarBlue = {
-    buy_price: buyPrice,
-    sell_price: sellPrice,
+    buy_price: data.compra,
+    sell_price: data.venta,
     source: "https://www.ambito.com/contenidos/dolar.html",
     name_page: "Ambito.com",
   };
-
-  await browser.close();
-
+  console.log(dataDolarBlue);
   return dataDolarBlue;
 }
 
